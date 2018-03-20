90ebf5e99bcde72830a51c8f34375e6ce8e6c4b2
Mohit Bansal <bansalmohit72@gmail.com>
2018-03-21 00:24:03 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,260 @@
+from flask import Flask, request
+from pymessenger import Bot
+from utils import fetch_reply, match_det, HELP_MSG,cur_match, data
+import requests,json
+import os
+
+app=Flask("Cricbot")
+FB_ACCESS_TOKEN="EAACIXuHyXOEBAAEWThayhIUc6JCMTZAEynAj9i7I5EjdsAYamR836JcoHheq7WduH4N1wJ2n4kxFifz3ZBTeczJwjQDhZA8cruJi854ykKJX1eQHJO35MxAftOt9Jnc6KvHBNBscXPZBAyyUQkhqVmduNoclIdOlx30c7w71MgZDZD"
+bot=Bot(FB_ACCESS_TOKEN)
+
+VERIFICATION_TOKEN="hello"
+
+@app.route('/',methods=['GET'])
+def verify():
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == VERIFICATION_TOKEN:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+    return "Hello world! This is Cricbot.", 200
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+	data = request.get_json()
+	#print(data)
+	if data['object'] == "page":
+		entries = data['entry']
+
+		for entry in entries:
+			messaging = entry['messaging']
+
+			for messaging_event in messaging:
+
+				sender_id = messaging_event['sender']['id']
+				if sender_id=='803956049791378':
+					return "ok",200
+				recipient_id = messaging_event['recipient']['id']
+
+				if messaging_event.get('message'):
+					try:
+						# HANDLE NORMAL MESSAGES HERE
+						if messaging_event['message'].get('text'):
+							# HANDLE TEXT MESSAGES
+							query = messaging_event['message']['text']
+							bot.send_action(sender_id,"mark_seen")
+							bot.send_action(sender_id,"typing_on")
+
+							if messaging_event['message'].get('quick_reply'):
+								# HANDLE TEXT MESSAGE WITH QUICK REPLY
+								payload = messaging_event['message']['quick_reply']['payload']
+								li=cur_match()
+								if li=="Error":
+									bot.send_text_message(sender_id,"Unable to process your request due to some technical issues. Please try again later.")
+									return "ok",200
+								for i in li:
+									if payload in i[1]:
+										query = payload
+										break
+							try :
+								#handle any error in utils.py
+								reply=fetch_reply(query,sender_id)
+							except Exception as e:
+								print(e)
+								print("here1\n")
+								reply={}
+								reply['type']="none"
+								reply['data']="Sorry"
+
+							if reply['type']=="match_detail":
+								#to show details of all live messages
+								#print(reply['data'])
+								bot.send_generic_message(sender_id, reply['data'])
+
+							elif reply['type']=="live_score":
+								#to show detail of a particular match
+								#print(reply['data'])
+								if "button" in reply.keys():
+									buttons = [{"type":"web_url",
+									"url": reply['button'],
+								    "title":"Scorecard"}]
+									bot.send_button_message(sender_id,reply['data'],buttons)
+								else:
+									bot.send_text_message(sender_id,reply['data'])
+
+							elif reply['type']=="match_info":
+								#print(reply['data'])
+								#to show info like venue, umpires, refree of a particular match
+								bot.send_text_message(sender_id,reply['data'])
+
+							elif reply['type']=='ranking_t':
+								#to show team rankings of test, ODI, T20i and women
+								s1=reply['data'][-1]+"\n\n"
+								excepti=['Zimbabwe',"Hong Kong"]
+								reply['data'].pop()
+								for i in reply['data']:
+									s=""
+									s+=str(i.get("rank"))
+									s+=" \t"
+									s+=i.get("team")
+									if len(i.get('team').rstrip(' Women'))<10 and i.get('team') not in excepti:
+										if "India Women" in i.get('team') or i.get('team')=="UAE" or i.get("team")=="PNG":
+											s+="\t"
+										s+="\t\t"
+									else:
+										s+="\t"
+									s+="  "
+									s+=i.get("rating")+"\n"
+									s1+=s
+								s1+="\nP.S.: Ratings in brackets\n"
+								bot.send_text_message(sender_id,s1)
+
+							elif reply['type']=="ranking_p":
+								# to show players ranking in odi, t20i and test in batting, bowling and all-rounder
+								s1=reply['data'][-1]+"\n\n"
+								reply['data'].pop()
+								button_url=reply['data'][-1]['button_url']
+								reply['data'].pop(-1)
+								for i in reply['data']:
+									s=""
+									s+=i.get("rank")
+									s+=" \t"
+									s+=i.get("player")
+									if len(i.get('player'))<10:
+										s+='\t\t'
+									else:
+										s+='\t'
+									s+=" "
+									s+=i.get("country")+" \t"
+									s+=i.get("rating")+"\n"
+									s1+=s
+								s1+="\nP.S.: Ratings in brackets\n"
+								buttons = [{"type":"web_url",
+									"url": button_url,
+								    "title":"Complete list"}]
+								bot.send_action(sender_id,"typing_off")
+								bot.send_button_message(sender_id,s1,buttons)
+							elif reply['type'] == 'none':
+								#to handle anything that bot doesn't understand
+								button=[{
+								"type":"postback",
+								"title":"Click here for help",
+								"payload":"help"
+								}]
+								bot.send_button_message(sender_id, "Sorry, I didn't understand.",button)
+
+							elif reply['type']=="offline":
+								#Status code error
+								bot.send_text_message(sender_id,reply['data'])
+
+							else:
+								#mainly to handle smalltalks
+								print(reply['data'])
+								bot.send_text_message(sender_id, reply['data'])
+						elif messaging_event['message'].get('attachments'):
+							#HANDLE ATTACHMENTS
+							try:
+								image_url=messaging_event['message']['attachments'][0]['payload']['url']
+								bot.send_image_url(sender_id,image_url)
+							except Exception as e:
+								print(e)
+								print("here2\n")
+					except Exception as e:
+						print(e)
+						print("here3\n")
+						bot.send_text_message(sender_id,"Unable to process your request due to some technical issues. Please try again later.")
+				
+				elif messaging_event.get("postback"):
+					payload=messaging_event['postback']['payload']
+					if payload=="match_detail":
+						data=match_det()
+						if data=="Error":
+							bot.send_text_message(sender_id,"Unable to process your request due to some technical issues. Please try again later.")
+						else:
+							bot.send_generic_message(sender_id,data)
+					elif payload=="live_score":
+						quick_reply=cur_match()[:10]
+						if quick_reply=="Error":
+							bot.send_text_message(sender_id,"Unable to process your request due to some technical issues. Please try again later.")
+						else:
+							bot.send_quickreply(sender_id,HELP_MSG,cur_match()[:10])
+					elif payload=='help':
+						bot.send_text_message(sender_id,"Hello I am Cricbot. I can show you details like live scores and match info of all the ongoing cricket matches.:)")
+						bot.send_text_message(sender_id,"For checking live scores, type 'team_name1 vs team_name2' and you will get live scores of that match.")
+						bot.send_text_message(sender_id,"For match info, type 'team_name1 vs team_name2 match info'.\nAnd for ICC team and player ranking, type <format> ranking.\nFor example: for test cricket ranking, type test ranking. ")
+						bot.send_text_message(sender_id,"If u still need any help, use persistent menu.")
+						button=[{
+						'type':'web_url',
+						'title':"Meet the developer",
+						'url':'https://www.facebook.com/MohitBansal97'
+						}]
+						bot.send_button_message(sender_id,"For any suggestion or query, contact my botmaster",button)
+	return "ok", 200
+	
+
+def set_greeting_text():
+    headers = {
+        'Content-Type':'application/json'
+        }
+    data = {
+        "setting_type":"greeting",
+        "greeting":{
+            "text":"Hi {{user_first_name}}! I am Cricbot. I can show you details like live scores and match info of all the ongoing cricket matches.:)"
+            }
+        }
+    ENDPOINT = "https://graph.facebook.com/v2.8/me/thread_settings?access_token=%s"%(FB_ACCESS_TOKEN)
+    r = requests.post(ENDPOINT, headers = headers, data = json.dumps(data))
+    #print(r.content)
+
+
+def set_persistent_menu():
+    headers = {
+        'Content-Type':'application/json'
+        }
+    data = {
+        "setting_type":"call_to_actions",
+        "thread_state" : "existing_thread",
+        "call_to_actions":[
+            {
+                "type":"web_url",
+                "title":"Meet the developer",
+                "url":"https://www.facebook.com/MohitBansal97" 
+            },{
+            "type":"postback",
+            "title":"Live Matches",
+            "payload":"match_detail"
+            },{
+            "type":"postback",
+            "title":"Live Scores",
+            "payload":"live_score"
+            },{
+            "type":"postback",
+            "title":"Help",
+            "payload":"help"
+            }]
+        }
+    ENDPOINT = "https://graph.facebook.com/v2.8/me/thread_settings?access_token=%s"%(FB_ACCESS_TOKEN)
+    r = requests.post(ENDPOINT, headers = headers, data = json.dumps(data))
+
+
+def get_started():
+    headers = {
+        'Content-Type':'application/json'
+        }
+    data = {
+        "setting_type":"call_to_actions",
+        "thread_state" : "new_thread",
+        "call_to_actions":[{
+            "type":"postback",
+            "payload":"help"
+            }]
+        }
+    ENDPOINT = "https://graph.facebook.com/v2.8/me/thread_settings?access_token=%s"%(FB_ACCESS_TOKEN)
+    r = requests.post(ENDPOINT, headers = headers, data = json.dumps(data))
+
+set_persistent_menu()
+set_greeting_text()
+get_started()
+
+if __name__=="__main__":
+	app.run(port=8000,use_reloader=True)
---
+++ utils.py
@@ -0,0 +1,448 @@
+import apiai
+import json
+from requests import *
+from bs4 import BeautifulSoup
+from datetime import date
+from pymongo import MongoClient
+
+#https://mohit-cricbot.herokuapp.com/
+
+APIAI_ACCESS_TOKEN = "daf07287853741709698f3d64f44b45a"
+
+MONGODB_URI = "mongodb://mohit:mohit@ds233748.mlab.com:33748/cricbot"
+client = MongoClient(MONGODB_URI)
+db = client.get_database("cricbot")
+data = db.data
+
+# a help message
+HELP_MSG = """
+Hey! I am Cricbot.:).
+Choose any one of the ongoing matches.:)
+"""
+
+cric_url="http://www.cricbuzz.com/cricket-match/live-scores"
+
+def insertion():
+	print("\n\nin insertion\n\n")
+	r=get(cric_url)
+	l="Error"
+	if r.status_code==200:
+		soup=BeautifulSoup(r.content,"html5lib")
+		matches=soup.findAll(class_="cb-col cb-col-100 cb-lv-main")
+		l=[]
+		for i in range(len(matches)):
+			a=matches[i].findAll("a")
+			teams=a[0].text
+			t1=teams.split(" vs ")[0]
+			t2=teams.split(" vs ")[1].rstrip(",")
+			l.append((t1.lower(),t2.lower()))
+	else:
+		url="http://www.espncricinfo.com/ci/engine/match/index.html?view=live"
+		r=get(url)
+		l=[]
+		if r.status_code==200:
+			soup=BeautifulSoup(r.content,"html5lib")
+			matches=soup.findAll("section",class_="default-match-block")
+			for match in matches:
+				preview=match.find('div',class_='innings-info-1')
+				ts1=preview.text.strip().replace("    "," ")
+				ts2=match.find('div',class_='innings-info-2').text.strip().replace("    "," ")
+				t1=ts1.strip(match.find('div',class_='innings-info-1').find('span').text)
+				t2=ts2.strip(match.find('div',class_='innings-info-2').find('span').text)
+				l.append((t1.lower(),t2.lower()))
+	return l
+def espn_scrape():
+	# if cricbuzz scraping has any error, then scrape from espncricinfo
+	url="http://www.espncricinfo.com/ci/engine/match/index.html?view=live"
+	cric_url=url
+	r=get(url)
+	l=[]
+	if r.status_code==200:
+		soup=BeautifulSoup(r.content,"html5lib")
+		matches=soup.findAll("section",class_="default-match-block")
+		for match in matches:
+			preview=match.find('div',class_='innings-info-1')
+			if preview.find('span').text=="":
+				continue
+			d={}
+			d['flag']=1
+			d['team_score1']=preview.text.strip().replace("    "," ")
+			d['team_score2']=match.find('div',class_='innings-info-2').text.strip().replace("    "," ")
+			d['result']=match.find('div',class_="match-status").text.strip()
+			d['team1']=d['team_score1'].strip(match.find('div',class_='innings-info-1').find('span').text)
+			d['team2']=d['team_score2'].strip(match.find('div',class_='innings-info-2').find('span').text)
+			d['scorecard_url']=match.find('a')['href'].replace('game','scorecard')
+			d['commentary_url']=match.find('a')['href'].replace('game','commentary')
+			d['match_facts_url']=match.find('a')['href']
+			l.append(d)
+	else:
+		l="Error"
+	return l
+
+def scrape():
+	"""
+	function to get live scores from cricbuzz
+	"""
+	r=get(cric_url)
+	if r.status_code==200:
+		soup=BeautifulSoup(r.content,"html5lib")
+		matches=soup.findAll(class_="cb-col cb-col-100 cb-lv-main")
+		l=[]
+		for i in range(len(matches)):
+			a=matches[i].findAll("a")
+			if "preview" in a[1]['class'][1]:
+				continue
+			s=matches[i].findAll("span")
+			d={}
+			d['flag']=0
+			teams=a[0].text
+			d['team1']=teams.split(" vs ")[0]
+			d['team2']=teams.split(" vs ")[1].rstrip(",")
+			score=a[1].text.split("\xa0•\xa0")
+			d['team_score1']=score[0].strip()
+			xyz=a[1].find(class_="cb-lv-scrs-col cb-text-live")
+			if xyz:
+				reslt=a[1].find(class_="cb-lv-scrs-col cb-text-live").text
+				team_score2=score[1].replace(reslt," ").strip()
+				d['team_score2']=team_score2
+				d['result']=reslt
+			else:
+				reslt=a[1].find(class_="cb-lv-scrs-col cb-text-complete").text
+				team_score2=score[1].replace(reslt," ").strip()
+				d['team_score2']=team_score2
+				d['result']=reslt
+			if "opt" in d['result']:
+				d['team_score1'],d['team_score2']=d['team_score2'],d['team_score1']
+			d['scorecard_url']="http://www.cricbuzz.com"+a[3]['href']
+			d['commentary_url']="http://www.cricbuzz.com"+a[4]['href']
+			d['match_facts_url']=d['commentary_url'].replace("full-commentary","match-facts")
+			l.append(d)
+	else:
+		l=espn_scrape()
+	return l
+
+
+def match_det():
+	scores = scrape()
+	if scores=="Error":
+		return scores
+	# create generic template
+	diff_matches = []
+	count=1
+	for match in scores:
+		if count>10:#max number of templates can only be 10
+			break
+		element = {}
+		element["title"] = match['team1']+" vs "+match['team2']
+		element['subtitle']=match['team_score2']+"\xa0•\xa0"+match['team_score1']+'\n'+match['result']
+		element["item_url"]=cric_url
+		element["buttons"] = [{
+			"type":"web_url",
+			"title":"Scorecard",
+			"url":match["scorecard_url"]},
+			{"type":"web_url",
+            "title":"Full Commentary",
+            "url":match['commentary_url']}]
+		diff_matches.append(element)
+		count+=1
+	return diff_matches
+
+
+def live_score(params):
+	l=scrape()
+	if l=="Error":
+		return l
+	#Check if match between given teams exists or not
+	teams=params.get("teams")
+	team1=teams[0].lower().strip()
+	team2=teams[1].lower().strip()
+	for i in l:
+		if i.get("team1").lower()==team1 or i.get("team2").lower()==team1:
+			if i.get("team1").lower()==team2 or i.get("team2").lower()==team2:
+				return i
+	return "error"
+
+
+def match_facts(params):
+	ls=live_score(params)
+	if ls=="Error" or ls=="error":
+		return ls
+	s=""
+	if ls["flag"]==0:
+		url=ls['match_facts_url']
+		r=get(url)
+		soup=BeautifulSoup(r.content,"html5lib")
+		info_h=soup.findAll(class_="cb-col cb-col-27 cb-mat-fct-itm text-bold")
+		info_b=soup.findAll(class_="cb-col cb-col-73 cb-mat-fct-itm")
+		for i in range(8):
+			s+="{} {}\n\n".format(info_h[i].text,info_b[i].text)
+	elif ls['flag']==1:
+		url=ls['match_facts_url']
+		r=get(url)
+		soup=BeautifulSoup(r.content,"html5lib")
+		info_h=soup.findAll("div",class_="match-detail--left")
+		info_b=soup.findAll("div",class_="match-detail--right")
+		s=""
+		for i in range(len(info_b)):
+		    s+=str(info_h[i].text)+" "
+		    a=info_b[i].findAll("span")
+		    for j in range(len(a)):
+		        if len(a)>1 and j!=len(a)-1:
+		            s+=str(a[j].text)+", "
+		        else:
+		            s+=str(a[j].text)
+		    s+="\n\n"
+	return s
+
+
+def cur_match():
+	l=scrape()
+	if l=="Error":
+		return l
+	#Creating quick reply
+	reply=[]
+	if l[0]['flag']==0:
+		for i in l:
+			team1=i.get("team_score1").split()[0]
+			team2=i.get("team_score2").split()[0]
+			match=team1+" vs "+team2
+			postback=i.get("team1") +" vs "+i.get("team2")+" live scores"
+			reply.append((match,postback))
+	elif l[0]['flag']==1:
+		for i in l:
+			team1=i.get("team1")
+			team2=i.get("team2")
+			match=team1+" vs "+team2
+			postback=i.get("team1") +" vs "+i.get("team2")+" live scores"
+			reply.append((match,postback))
+	return reply
+
+
+def ranking_t(params):
+	#function to team ranking from espncricinfo
+	match_type=["Test","ODI","T20i"]
+	url="http://www.espncricinfo.com/rankings/content/page/211271.html"
+	r=get(url)
+	if r.status_code==200:
+
+		soup=BeautifulSoup(r.content,"html5lib")
+		table=soup.findAll('table')
+		tables_t=[]
+		for i in range(len(table)):
+			tbody=table[i].find("tbody")
+			team=tbody.findAll("tr")
+			li=[]
+			for j in range(1,len(team)):
+				data=team[j].findAll("td")
+				d={}
+				d['rank']=j
+				d['team']=data[0].text
+				d['rating']="({})".format(data[3].text)
+				li.append(d)
+			tables_t.append(li)
+		x=params.get('Type')
+		if len(x)==0:
+			x.append("Test")
+		if "Women" in x:
+			tables_t[3].append("Women's Rankings")
+			l=tables_t[3]
+			return l
+		for i in range(3):
+			if match_type[i] in x:
+				tables_t[i].append(match_type[i]+" Ranking")
+				l=tables_t[i]
+				return l
+	else:
+		return "Error"
+
+def ranking_p(params):
+	#Function to scrape player ranking from espncricinfo
+	url="http://www.espncricinfo.com/rankings/content/page/211270.html"
+	r=get(url)
+	if r.status_code==200:
+		soup=BeautifulSoup(r.content,"html5lib")
+		frames=soup.findAll('iframe')
+		tables_p=[]
+		button_url="https://www.icc-cricket.com/rankings/mens/player-rankings/test/batting"
+		li=[("mens","test","batting"),("mens","test","bowling"),("mens","test","all-rounder"),("mens","odi","batting"),("mens","odi","bowling"),
+		("mens","odi","all-rounder"),("mens","t20i","batting"),("mens","t20i","bowling"),("mens","t20i","all-rounder"),("womens","odi","batting"),
+		("womens","odi","bowling"),("womens","odi","all-rounder"),("womens","t20i","batting"),("womens","odi","bowling"),
+		("womens","t20i","all-rounder")]
+		x=params.get("Type")
+		i=0
+		heading=""
+		if "Women" in x:
+			if "ODI" not in x and "T20i" not in x:
+				x.append("ODI")
+			if "Batting" not in x and "Bowling" not in x and "All-rounder" not in x:
+				x.append("Batting")
+			if "Test" in x:
+				x.replace("Test","ODI")
+			x=list(map(lambda x:x.lower(),x))
+			for i in range(9,15):
+				if li[i][1] in x and li[i][2] in x :
+					break
+			heading="Women's "+li[i][1].capitalize()+" "+li[i][2].capitalize()+" Ranking"
+		else:
+			if "ODI" not in x and "T20i" not in x and "Test" not in x:
+				x.append("Test")
+			if "Batting" not in x and "Bowling" not in x and "All-rounder" not in x:
+				x.append("Batting")
+			x=list(map(lambda x:x.lower(),x))
+			for i in range(0,9):
+				if li[i][1] in x and li[i][2] in x :
+					break
+			heading=li[i][1].capitalize()+" "+li[i][2].capitalize()+" Ranking"
+		r1=get(frames[i]['src'])
+		if r1.status_code==200:
+			sp=BeautifulSoup(r1.content,"html5lib")
+			tb=sp.findAll('tr')
+			l=[]
+			for j in range(2,len(tb)-1):
+				d={}
+				td=tb[j].findAll('td')
+				d['rank']=td[0].text
+				d['player']=td[1].text
+				d['country']=td[2].text
+				d['rating']="({})".format(td[3].text)
+				l.append(d)
+			req_url=button_url.replace("mens",li[i][0])
+			req_url=req_url.replace("test",li[i][1])
+			req_url=req_url.replace("batting",li[i][2])
+			d={}
+			d['button_url']=req_url
+			l.append(d)
+			heading=heading.replace("Odi","ODI")
+			l.append(heading)
+			return l
+		else:
+			return "Error"
+	else:
+		return "Error"
+
+def apiai_response(query, session_id):
+	"""
+	function to fetch api.ai response
+	"""
+	
+	ai = apiai.ApiAI(APIAI_ACCESS_TOKEN)
+	request = ai.text_request()
+	request.lang='en'
+	request.session_id=session_id
+	request.query = query
+	response = request.getresponse()
+	return json.loads(response.read().decode('utf8'))
+
+
+def parse_response(response):
+	"""
+	function to parse response and
+	return intent and its parameters
+	"""
+	result = response['result']
+	params = result.get('parameters')
+	intent = result['metadata'].get('intentName')
+	return intent, params
+
+
+def fetch_reply(query, session_id):
+	"""
+	main function to fetch reply for chatbot and
+	return a reply dict with reply 'type' and 'data'
+	"""
+	response = apiai_response(query, session_id)
+	intent, params = parse_response(response)
+	print(intent,params)
+	if params.get('teams'):
+		l_team=[]
+		for i in params['teams']:
+			l_team.append(i.strip())
+		params['teams']=l_team
+	reply = {}
+	d=data.find_one({'date':str(date.today())})
+	if d is None:
+		d1={}
+		d1['date']=str(date.today())
+		l=insertion()
+		d1['data']=l
+		if l!="Error":
+			data.insert_one(d1)
+		d=d1
+
+	if response['result']['action'].startswith('smalltalk') or intent=="Default Welcome Intent":
+		reply['type'] = 'smalltalk'
+		reply['data'] = response['result']['fulfillment']['speech']
+
+	elif intent == "match_detail":
+		reply['type'] = 'match_detail'
+		templates=match_det()
+		if templates=="Error":
+			reply['type']="offline"
+			reply['data']="Unable to process your request due to some technical issues. Please try again later."
+		else:
+			reply["data"] = templates
+
+	elif intent=="live_score":
+		reply['type']="live_score"
+		teams=params.get('teams')
+		if len(teams)==2:
+			team1=teams[0].lower()
+			team2=teams[1].lower()
+			if d['data']=="Error":
+				score="Error"
+			else:
+				for match in d['data']:
+					if (team1 in match[0] or team2 in match[0]) and (team2 in match[1] or team1 in match[1]):
+						score=live_score(params)
+						break
+				else:
+					score="error"
+		else:
+			score='Error'
+		if score=="Error":
+			reply['type']="offline"
+			reply['data']="Unable to process your request due to some technical issues. Please try again later."
+		elif score=="error":
+			teams=params.get("teams")
+			reply['data']="Sorry, there is no match between {} and {}".format(teams[0],teams[1])
+		else:
+			reply['data']="{} vs {}\n\n{}\n{}\n\n{}".format(score['team1'],score['team2'],score['team_score2'],score['team_score1'],score['result'])
+			reply['button']=score['scorecard_url']
+
+	elif intent=="match_info":
+		s=match_facts(params)
+		reply['type']="match_info"
+		if info_b=="Error":
+			reply['type']="offline"
+			reply['data']="Unable to process your request due to some technical issues. Please try again later."
+		if info_h=="error":
+			teams=params.get("teams")
+			reply['data']="Sorry, no match between {} and {}".format(teams[0],teams[1])
+		else:
+			reply['data']=s
+
+	elif intent=="ranking":
+		reply['type']="ranking_t"
+		x=params.get('Type')
+		if "Player" in params.get('Type') or "Batting" in x or "Bowling" in x or "All-rounder" in x :
+			if "Player" not in x:
+				params['Type']+=["Player"]
+			reply['type']="ranking_p"
+			rank=ranking_p(params)
+			if rank=="Error":
+				reply['type']="offline"
+				reply['data']="Unable to process your request due to some technical issues. Please try again later."
+			else:
+				reply['data']=rank
+		else:
+			rank=ranking_t(params)
+			if rank=="Error":
+				reply['type']="offline"
+				reply['data']="Unable to process your request due to some technical issues. Please try again later."
+			else:
+				reply['data']=rank
+	else:
+		reply["type"] = 'none'
+		reply["data"] = ""
+
+	return reply
