97e33766cae99f27f93fdf2cc70901d1c1e827e7
Mohit Bansal <bansalmohit72@gmail.com>
2024-10-11 01:17:03 +0530

    Updated cricbot app

--- app/cricbot_app.py
+++ app/cricbot_app.py
@@ -1,7 +1,13 @@
 import os
 from dotenv import find_dotenv, load_dotenv
 import streamlit as st
-from services.cricbot_service import CricbotService
+from services import CricbotService
+
+# Define avatars for assistant and user
+avatars = {
+    "assistant": "🏏",  # Cricket bat and ball emoji for the assistant
+    "user": "🙋‍♂️"       # Person raising hand emoji for the user
+}
 
 def initialize_environment():
     """
@@ -47,7 +53,7 @@ def display_initial_messages():
         st.session_state["messages"] = [{"role": "assistant", "content": "Hi! I am Cricbot. You can ask me questions about cricket"}]
 
     for msg in st.session_state.messages:
-        st.chat_message(msg["role"]).write(msg["content"])
+        st.chat_message(msg["role"], avatar=avatars[msg["role"]]).write(msg["content"])
 
 def handle_user_input(cricbot_service: CricbotService):
     """
@@ -59,22 +65,27 @@ def handle_user_input(cricbot_service: CricbotService):
         An instance of CricbotService to generate responses.
     """
     if prompt := st.chat_input():
-        st.session_state.messages.append({"role": "human", "content": prompt})
-        st.chat_message("human").write(prompt)
-        try:
-            response = cricbot_service.bot_response(prompt)
-            st.session_state.messages.append({"role": "assistant", "content": response})
-            st.chat_message("assistant").write(response)
-        except Exception as e:
-            error_message = f"Error generating response: {str(e)}"
-            st.session_state.messages.append({"role": "assistant", "content": error_message})
-            st.chat_message("assistant").write(error_message)
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user", avatar=avatars["user"]).write(prompt)
+        
+        # Create a placeholder for the loading spinner
+        with st.spinner("Generating response..."):
+            try:
+                response = cricbot_service.bot_response(prompt)
+                st.session_state.messages.append({"role": "assistant", "content": response})
+                st.chat_message("assistant", avatar=avatars["assistant"]).write(response)
+            except Exception as e:
+                print(e)
+                error_message = "Error generating response"
+                st.session_state.messages.append({"role": "assistant", "content": error_message})
+                st.chat_message("assistant", avatar=avatars["assistant"]).write(error_message)
 
 def main():
     """
     Main function to run the Streamlit application.
     """
-    st.title("💬 Cricbot")
+    st.set_page_config(page_title="Cricbot", page_icon="🏏")
+    st.title("🏏 Cricbot")
     initialize_environment()
     api_key = get_openai_api_key()
     cricbot_service = initialize_chatbot(api_key)
